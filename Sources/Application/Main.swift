11ec71685d1404a2c35faf5746d8a1fd9aae6700
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2026-07-27 16:55:40 +0200

    Refactor LED control in swift_main function to use individual pins for red, yellow, and green LEDs

--- Sources/Application/Main.swift
+++ Sources/Application/Main.swift
@@ -3,14 +3,32 @@
 
 @_cdecl("swift_main")
 public func swift_main() -> Never {
-    let ledPin = PicoBoard.onboardLEDPin
-    RP2040GPIO.configureAsSIOOutput(pin: ledPin)
+    let redPin = UInt32(15)
+    let yellowPin = UInt32(14)
+    let greenPin = UInt32(12)
+
+    RP2040GPIO.configureAsSIOOutput(pin: redPin)
+    RP2040GPIO.configureAsSIOOutput(pin: yellowPin)
+    RP2040GPIO.configureAsSIOOutput(pin: greenPin)
 
     while true {
-        RP2040GPIO.setHigh(pin: ledPin)
-        pico_delay_ms(250)
-        RP2040GPIO.setLow(pin: ledPin)
-        pico_delay_ms(250)
+        // Red on, others off
+        RP2040GPIO.setHigh(pin: redPin)
+        RP2040GPIO.setLow(pin: yellowPin)
+        RP2040GPIO.setLow(pin: greenPin)
+        pico_delay_ms(5000)
+
+        // Yellow on, others off
+        RP2040GPIO.setLow(pin: redPin)
+        RP2040GPIO.setHigh(pin: yellowPin)
+        RP2040GPIO.setLow(pin: greenPin)
+        pico_delay_ms(2000)
+
+        // Green on, others off
+        RP2040GPIO.setLow(pin: redPin)
+        RP2040GPIO.setLow(pin: yellowPin)
+        RP2040GPIO.setHigh(pin: greenPin)
+        pico_delay_ms(5000)
     }
 }
 
