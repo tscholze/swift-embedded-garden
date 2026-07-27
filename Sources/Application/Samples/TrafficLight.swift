5ff42d1a0f93f3e1d269252ae000087cb4d48801
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2026-07-27 20:05:54 +0200

    Refactor TrafficLight struct to improve GPIO pin configuration and enhance blink sequence logic

--- Sources/Application/Samples/TrafficLight.swift
+++ Sources/Application/Samples/TrafficLight.swift
@@ -1,47 +1,68 @@
 struct TrafficLight {
+    // MARK: - GPIO Pin Assignments -
+
+    private let ledPin = PicoBoard.onboardLEDPin
+    private let redPin = UInt32(2)
+    private let yellowPin = UInt32(3)
+    private let greenPin = UInt32(4)
+
     // MARK: - Sample -
 
     func run() -> Never {
+        RP2040GPIO.configureAsSIOOutput(pin: ledPin)
+        RP2040GPIO.configureAsSIOOutput(pin: redPin)
+        RP2040GPIO.configureAsSIOOutput(pin: yellowPin)
+        RP2040GPIO.configureAsSIOOutput(pin: greenPin)
+
         blinkSequence()
     }
 
     // MARK: - Traffic Light Simulation -
 
     private func blinkSequence() -> Never {
-        let ledPin = PicoBoard.onboardLEDPin
-
-        let redPin = UInt32(2)
-        let yellowPin = UInt32(3)
-        let greenPin = UInt32(4)
-
-        RP2040GPIO.configureAsSIOOutput(pin: ledPin)
-        RP2040GPIO.configureAsSIOOutput(pin: redPin)
-        RP2040GPIO.configureAsSIOOutput(pin: yellowPin)
-        RP2040GPIO.configureAsSIOOutput(pin: greenPin)
-
         while true {
-            // Indicate a new cycle
-            RP2040GPIO.setHigh(pin: ledPin)
-            pico_delay_ms(250)
-            RP2040GPIO.setLow(pin: ledPin)
+            showCycleIndicator()
 
-            // Red on, others off
-            RP2040GPIO.setHigh(pin: redPin)
-            RP2040GPIO.setLow(pin: yellowPin)
-            RP2040GPIO.setLow(pin: greenPin)
+            showRed()
             pico_delay_ms(1000)
-
-            // Yellow on, others off
-            RP2040GPIO.setLow(pin: redPin)
-            RP2040GPIO.setHigh(pin: yellowPin)
-            RP2040GPIO.setLow(pin: greenPin)
+            showYellow()
             pico_delay_ms(1000)
-
-            // Green on, others off
-            RP2040GPIO.setLow(pin: redPin)
-            RP2040GPIO.setLow(pin: yellowPin)
-            RP2040GPIO.setHigh(pin: greenPin)
+            showGreen()
             pico_delay_ms(1000)
         }
+    }
+
+    private func blinkWithKnob() -> Never {
+        while true {
+            showCycleIndicator()
+
+            pico_delay_ms(250)
+        }
+    }
+
+    // MARK: - Helper -
+
+    private func showCycleIndicator() {
+        RP2040GPIO.setHigh(pin: ledPin)
+        pico_delay_ms(250)
+        RP2040GPIO.setLow(pin: ledPin)
+    }
+
+    private func showRed() {
+        RP2040GPIO.setHigh(pin: redPin)
+        RP2040GPIO.setLow(pin: yellowPin)
+        RP2040GPIO.setLow(pin: greenPin)
+    }
+
+    private func showYellow() {
+        RP2040GPIO.setLow(pin: redPin)
+        RP2040GPIO.setHigh(pin: yellowPin)
+        RP2040GPIO.setLow(pin: greenPin)
+    }
+
+    private func showGreen() {
+        RP2040GPIO.setLow(pin: redPin)
+        RP2040GPIO.setLow(pin: yellowPin)
+        RP2040GPIO.setHigh(pin: greenPin)
     }
 }
