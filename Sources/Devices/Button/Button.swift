ee1d4179cc87215ac4f7312927e4ac5fbb8714c7
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2026-08-05 19:05:45 +0200

    feat: add Button support with configuration and press detection

---
+++ Sources/Devices/Button/Button.swift
@@ -0,0 +1,21 @@
+struct Button {
+
+  private let configuration: ButtonConfiguration
+
+  init(configuration: ButtonConfiguration) {
+    self.configuration = configuration
+    configure()
+  }
+
+  private func configure() {
+    RP2040GPIO.configureAsSIOInput(pin: configuration.triggerPin)
+  }
+
+  func isPressed() -> Bool {
+    return RP2040GPIO.read(pin: configuration.triggerPin)
+  }
+}
+
+struct ButtonConfiguration {
+  let triggerPin: UInt32
+}
